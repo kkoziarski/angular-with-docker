a6b93677d207d6a0e9ff86d9e02d409711b6846d
Krzysztof Koziarski <krzyhook@gmail.com>
2018-07-10 07:20:11 +0200

    fix docker-run.bat - unnecessary quotes

--- .docker/tools/docker-run.bat
+++ .docker/tools/docker-run.bat
@@ -5,8 +5,8 @@
 ::!!Note: Make sure put the container name after the environment variable, not before that.
 
 docker run --rm -it -p 34444:80^
-  -e ENV_NAME='localInContainer'^
-  -e API_HOST_URL='//localhost:33333/'^
-  -e ADAL_TENANT='ooaaaooo.onmicrosoft.com'^
-  -e ADAL_CLIENT_ID='00020000-0000-1111-0000-000000003000'^
+  -e ENV_NAME=localInContainer^
+  -e API_HOST_URL=//localhost:33333/^
+  -e ADAL_TENANT=ooaaaooo.onmicrosoft.com^
+  -e ADAL_CLIENT_ID=00020000-0000-1111-0000-000000003000^
   kk/ng-docker:v1
